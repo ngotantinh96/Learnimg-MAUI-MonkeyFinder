38f2be31ecb042a50690ba55b942ad6fa4d7a6b8
ngotantinh96 <ngotantinh96@gmail.com>
2024-04-03 16:23:33 +0700

    feat: display list of monkeys from API with MVVM

--- MonkeyFinder/Services/MonkeyService.cs
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,36 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    private readonly HttpClient httpClient;
+
+    private List<Monkey> monkeys = [];
+
+    public MonkeyService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<List<Monkey>> GetMonkeysAsync()
+    {
+        if (monkeys?.Count > 0)
+            return monkeys;
+
+        var url = "https://montemagno.com/monkeys.json";
+        var response = await httpClient.GetAsync(url);
+
+        if(response.IsSuccessStatusCode)
+        {
+            monkeys = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        //using var stream = await FileSystem.OpenAppPackageFileAsync("monkeys.json");
+        //using var reader = new StreamReader(stream);
+        //var contents = await reader.ReadToEndAsync();
+        //monkeys = JsonSerializer.Deserialize<List<Monkey>>(contents);
+
+        return monkeys;
+    }
 }
--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,47 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    private readonly MonkeyService monkeyService;
+
+    public ObservableCollection<Monkey> Monkeys { get; } = [];
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        this.monkeyService = monkeyService;
+        Title = "Monkey Finder";
+    }
+
+    [RelayCommand]
+    public async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            var monkeys = await monkeyService.GetMonkeysAsync();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get monkeys: {ex.Message}", "Ok!");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
